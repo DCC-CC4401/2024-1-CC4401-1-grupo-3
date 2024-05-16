08bd16977acdfe5112ba9ffad5672746f7b00b85
AsessionGuy <gallardo.andrescornejo@gmail.com>
2024-05-16 17:23:23 -0400

    fixed models implementation. Added model for NuevoReporteForm.

--- estadobuxef/models.py
+++ estadobuxef/models.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -14,20 +15,29 @@ class Estudiante(UsuarioRegistrado):
 
 
 class Reporte(models.Model):
+    hora = models.DateTimeField(auto_now_add=True)
+    contenido = models.TextField()
+    lugar = models.ForeignKey('Lugar', on_delete=models.PROTECT)
+    image = models.FileField(upload_to='uploads/estudiante/', blank=True, null=True)
+
     def __init__(self, *args, **kwargs):
         super(Reporte, self).__init__(*args, **kwargs)
-        self.hora = models.DateTimeField(auto_now_add=True)
-        self.contenido = models.TextField()
-        self.lugar = models.ForeignKey('Lugar', on_delete=models.PROTECT)
 
 
 class Lugar(models.Model):
+    categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT, default='Sin categoria')
     def __init__(self, *args, **kwargs):
         super(Lugar, self).__init__(*args, **kwargs)
-        self.categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT)
 
 
 class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, null=False, db_default='Sin categoría')
     def __init__(self, *args, **kwargs):
         super(Categoria, self).__init__(*args, **kwargs)
-        self.nombre = models.CharField(max_length=50)
+
+
+class NuevoReporteForm(forms.ModelForm):
+
+    class Meta:
+        model = Reporte
+        fields = ['contenido', 'lugar']
--- estadobuxef/views.py
+++ estadobuxef/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import NuevoReporteForm
+
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form_reporte = NuevoReporteForm(request.POST)
+        if form_reporte.is_valid():
+            reporte = form_reporte.cleaned_data
+            reporte.save()
+    elif request.method == "GET":
+        return render(request, "home.html", {'lugares': [["F10", "0"]]})
+
 
 def login(request):
     return render(request, "login.html")
+
 
 def register(request):
     return render(request, "register.html")
