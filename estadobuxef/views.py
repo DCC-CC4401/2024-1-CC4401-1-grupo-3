c70994c4e303867d6ab4083539a2b26313534e82
AsessionGuy <gallardo.andrescornejo@gmail.com>
2024-05-22 12:42:16 -0400

    fixed `log_reg` view returning None when a form (login or register) is invalid.

--- estadobuxef/views.py
+++ estadobuxef/views.py
@@ -5,8 +5,10 @@ from .models import NuevoReporteForm, Lugar, Reporte
 from django.contrib.auth import login, authenticate, logout
 from .forms import LoginForm, RegisterForm
 
+
 # Create your views here.
 def home(request):
+
     if request.method == "POST":
         form_reporte = NuevoReporteForm(request.POST)
         if form_reporte.is_valid():
@@ -20,24 +22,32 @@ def home(request):
 
 
 def log_reg(request):
+
+    login_form = LoginForm()
+    register_form = RegisterForm()
+
+    if request.method == 'POST' and 'login-form' not in request.POST and 'signup-form' not in request.POST:
+        messages.error(request, f'Invalid username or password')
+        return render(request, 'log-reg.html', {'login_form': login_form, 'register_form': register_form})
+
     if request.method == 'POST' and 'login-form' in request.POST:
         form = LoginForm(request.POST)
-    
+
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request,username=username,password=password)
+            user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                messages.success(request,f'Hi {username.title()}, welcome back!')
+                messages.success(request, f'Hi {username.title()}, welcome back!')
                 return HttpResponseRedirect('/')
-        
+
         # form is not valid or user is not authenticated
-        messages.error(request,f'Invalid username or password')
-        return render(request,'log-reg.html',{'login_form': login_form, 'register_form': register_form})
-        
-    if request.method=='POST' and 'signup-form' in request.POST:
-        form = RegisterForm(request.POST) 
+        messages.error(request, f'Invalid username or password')
+        return render(request, 'log-reg.html', {'login_form': login_form, 'register_form': register_form})
+
+    if request.method == 'POST' and 'signup-form' in request.POST:
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -47,13 +57,12 @@ def log_reg(request):
             return HttpResponseRedirect('/')
         else:
             return render(request, 'log-reg.html', {'login_form': login_form, 'register_form': register_form})
-            
     elif request.method == "GET":
         login_form = LoginForm()
         register_form = RegisterForm()
-        return render(request,'log-reg.html', {'login_form': login_form, 'register_form': register_form})
+        return render(request, 'log-reg.html', {'login_form': login_form, 'register_form': register_form})
+
 
 def reports(request):
     if request.method == "GET":
         return render(request, "reports.html", {'data': Reporte.objects.all()})
-
