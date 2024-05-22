5e22246e8316b31b434a63c61bfc0e195a6a0838
tagsqz <taracena@dcc.uchile.cl>
2024-05-21 21:00:11 -0400

    changes in log_reg view to show correct form
    changed the render parameters to receive both forms, changed its names
    in the html file, created a forms.py where the forms are

--- estadobuxef/views.py
+++ estadobuxef/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import NuevoReporteForm, Lugar, Reporte
+from .forms import LoginForm, RegisterForm
+
 
 
 # Create your views here.
@@ -18,7 +21,38 @@ def home(request):
 
 
 def log_reg(request):
-    return render(request, "log-reg.html")
+    if request.method == 'POST' and 'login-form' in request.POST:
+        form = LoginForm(request.POST)
+    
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                login(request, user)
+                messages.success(request,f'Hi {username.title()}, welcome back!')
+                return HttpResponseRedirect('/')
+        
+        # form is not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'log-reg.html',{'login_form': login_form, 'register_form': register_form})
+        
+    if request.method=='POST' and 'signup-form' in request.POST:
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'log-reg.html', {'login_form': login_form, 'register_form': register_form})
+            
+    elif request.method == "GET":
+        login_form = LoginForm()
+        register_form = RegisterForm()
+        return render(request,'log-reg.html', {'login_form': login_form, 'register_form': register_form})
 
 def reports(request):
     if request.method == "GET":
